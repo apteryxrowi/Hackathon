eb8e999c710e07e77ffa6e0b8150133a28662ae6
apteryxrowi <kevindakiwi6000@gmail.com>
2026-03-23 15:20:06 -0400

    normalized units? start blackhole implementation

--- cro/project.py
+++ cro/project.py
@@ -1,21 +1,31 @@
 import pygame
 from ui_controls import *
 import math
-global mass, planet, G, dt, GRID_DENSITY, angle, magnitude
+global mass, planet, G, dt, GRID_DENSITY, angle, magnitude, PI, C
+PI = 3.14159265358979
 GRID_DENSITY = 50
 MASS_DENSITY = 0.1
 dt = 1
+C = 299792458
+# supposed to be 6.6743e-11
 G = 100
 mass = 10
 angle = 0
 magnitude = 0
-planet = []
+bodies = []
 class Body:
-    def __init__(self, position, velocity, density, mass):
+    def __init__(self, position, velocity, density, mass, rotation):
         self.position = position
+        # in m/s
         self.velocity = velocity
+        # in kg/m^3
         self.density = density
+        # in m/s^2
+        self.rotation = rotation
+        # in kgs
         self.mass = mass
+        # in meters
+        self.radius = (3*(self.mass/self.density)/4/PI)**(1/3)
         self.x = position[0]
         self.y = position[1]
     def setVelocity(self, x, y):
@@ -23,45 +33,43 @@ class Body:
     def checkCollision(self, p):
         if (self == p):
             return
-        if distance(self.position, p.position) <= (1/(self.density/self.mass)/2):
+        if distance(self.position, p.position) <= self.radius:
             self.collide(p)
     def collide(self, p):
         pass
     def update(self):
-
         self.position[0] += self.velocity[0] * dt
         self.position[1] += self.velocity[1] * dt
         self.x = self.position[0]
         self.y = self.position[1]
+        self.radius = (3*(self.mass/self.density)/4/PI)**(1/3)
 
-class Planet(Body):
-    def __init__(self, planet_density, location, mass, velocity, screen):
-        super().__init__(location, velocity, planet_density, mass)
-        self.planet_size = 1/(self.density/self.mass)
+class Star(Body):
+    def __init__(self, density, location, mass, velocity, rotation, screen):
+        super().__init__(location, velocity, density, mass, rotation)
         self.screen = screen
     def collide(self, p):
-        if isinstance(p, Planet):
+        if isinstance(p, Star):
             self.position = [(self.position[0]+p.position[0])/2, (self.position[1]+p.position[1])/2]
-            self.velocity = [(self.velocity[0]+p.velocity[0])/2, (self.velocity[1]+p.velocity[1])/2]
+            self.velocity = [(self.velocity[0]*self.mass+p.velocity[0]*p.mass)/(2*(self.mass+p.mass)), (self.velocity[1]*self.mass+p.velocity[1]*p.mass)/(2*(self.mass+p.mass))]
             self.mass += p.mass
-            self.mass = min(250, self.mass)
-            self.planet_size = 1/(self.density/self.mass)
-            planet.remove(p)
+            # self.mass = min(250, self.mass)
+            self.radius = (3*(self.mass/self.density)/4/PI)**(1/3)
+            bodies.remove(p)
     def draw(self):
         print("there")
-        pygame.draw.circle(self.screen, "yellow", (self.x, self.y), radius = self.planet_size//2)
+        pygame.draw.circle(self.screen, "yellow", (self.x, self.y), radius = self.radius)
     def update(self):
         super().update()
-        self.planet_size = 1/(self.density/self.mass)
 
-class Blackhole:
-    def __init__(self, radius, position, rotation):
-        self.radius = radius
-        self.position = position
-        self.rotation = rotation
-
+class Blackhole(Body):
+    def __init__(self, position, velocity, mass, rotation, screen):
+        super().__init__(position, velocity, 1/(mass**2), mass, rotation)
+        # schwarzchild radius
+        self.radius = 2*G*self.mass/C**2
+        self.screen = screen
     def draw():
-        pass
+        pygame.draw.circle(self.screen, "black", (self.x, self.y), radius = self.radius)
     
 class Photon:
     def __init__(self, position, velocity):
@@ -90,25 +98,25 @@ def distance(positionA, positionB):
     return (((positionA[0] - positionB[0])**2) + ((positionA[1] - positionB[1])**2))**(1/2)
 
 
-def create_planet(planet_density, location, mass, velocity, screen):
-    new_planet = Planet(planet_density, location, mass, velocity, screen)
-    planet.extend([new_planet])
+def create_star(density, location, mass, velocity, screen):
+    new_star = Star(density, location, mass, velocity, 0, screen)
+    bodies.extend([new_star])
 
 def change_mass(value):
     global mass
     mass = value
-
 def change_angle(value):
     global angle
     angle = value
 def reset_sim():
-    planet.clear()
+    bodies.clear()
 def change_magnitude(value):
     global magnitude
     magnitude = value
 def begin_sim():
     global is_title_screen
     is_title_screen = False
+
 def main():
     global is_title_screen
     is_title_screen = True
@@ -119,7 +127,7 @@ def main():
     pygame.display.set_caption("Universe Simulation")
     clock = pygame.time.Clock()
     font = pygame.font.SysFont("Comic Sans MS", 15)
-    m_slider = Slider(800, 200, 100, 20, 10, 100, color = "white", text = "Mass Slider", font = font, on_change = change_mass, continuous=True)
+    m_slider = Slider(800, 200, 100, 20, 10, 10000, color = "white", text = "Mass Slider", font = font, on_change = change_mass, continuous=True)
     angle_slider = Slider(800, 240, 100, 20, 0, 360, color = "white", text ="Angle", font = font, on_change = change_angle, continuous=True)
     magnitude_slider =  Slider(800, 280, 100, 20, 0, 20, color = "white", text ="Magnitude", font = font, on_change = change_magnitude, continuous=True)
     
@@ -136,13 +144,10 @@ def main():
                 if event.key == pygame.K_r:
                     reset_sim()
             for w in widgets:
-                
                 w.handle_event(event)
-            
-
             if event.type == pygame.MOUSEBUTTONUP and event.pos[0] < 600:
                 x, y = event.pos
-                create_planet(1, [x, y], mass, [magnitude*math.cos(angle*0.0174533), magnitude*math.sin(angle*0.0174533)], screen)
+                create_star(1, [x, y], mass, [magnitude*math.cos(angle*0.0174533), magnitude*math.sin(angle*0.0174533)], screen)
         # Draw widgets after background
         if not is_title_screen:
             for w in widgets:
@@ -156,30 +161,31 @@ def main():
                 for j in range(-1, (HEIGHT)//GRID_DENSITY + 1):
                     x = (i*GRID_DENSITY)+(GRID_DENSITY//2)
                     y = (j*GRID_DENSITY)+(GRID_DENSITY//2)
-                    grav = gravity([x,y], planet, type = "gridline")
-                    scale = 10
-                    x += grav[0]*scale
-                    y += grav[1]*scale
+                    grav = gravity([x,y], bodies, type = "gridline")
+                    scale = 0.1
+                    dx = grav[0] * scale
+                    dy = grav[1] * scale
+                    x += min(dx, GRID_DENSITY) if dx >= 0 else max(dx, -GRID_DENSITY)
+                    y += min(dy, GRID_DENSITY) if dy >= 0 else max(dy, -GRID_DENSITY)
                     point = (x, y)
                     temp.append(point)
                     pygame.draw.circle(screen, (0, min(max(255-i*15, 0), 255), min(max(255-j*15, 0), 255)), (x, y), radius = 2)
                 points.append(temp)
-                print(points)
-            
-
+                # print(points)
             for row in range(len(points)):
                 for col in range(len(points[row])):
                     pygame.draw.line(screen, "lightblue", (points[row][col][0], points[row][col][1]), (points[min(row+1, len(points)-1)][col][0], points[min(row+1, len(points)-1)][col][1]))
                     pygame.draw.line(screen, "lightblue", (points[row][col][0], points[row][col][1]), (points[row][min(col+1, len(points[row])-1)][0], points[row][min(col+1, len(points[row])-1)][1]))
-            previousPlanets = planet
+            # update planets
+            previousBodies = bodies
             px = 0
             py = 0
             totalMass = 0
-            for p in planet:
-                for pp in planet:
+            for p in bodies:
+                for pp in bodies:
                     p.checkCollision(pp)
-            for i, p in enumerate(planet):
-                grav = gravity(p.position, previousPlanets, ind=i)
+            for i, p in enumerate(bodies):
+                grav = gravity(p.position, previousBodies, ind=i)
                 p.velocity[0] += grav[0] * dt
                 p.velocity[1] += grav[1] * dt
                 px += p.position[0] * p.mass
@@ -188,7 +194,8 @@ def main():
                 p.update()
                 p.draw()
                 print(p.x, p.y, p.velocity, p.position)
-            pygame.draw.circle(screen, "white", (px/max(totalMass, 1), py/max(totalMass, 1)), radius=5)
+            # center of mass
+            pygame.draw.circle(screen, "white", (px/max(totalMass, 1), py/max(totalMass, 1)), radius=totalMass**0.1)
         else:
             drawText(screen, "Spacetime Simulation", size = 30)
             drawText(screen, "Created by: Kevin Wang and Ubayd Subahan", x_offset = -35, size = 35)
@@ -196,7 +203,6 @@ def main():
             drawText(screen, "Click to begin", x_offset=-100, size = 20)
             for event in pygame.event.get():
                 if event.type == pygame.MOUSEBUTTONUP:
-                    
                     is_title_screen = False
         pygame.display.flip()
         clock.tick(60)
